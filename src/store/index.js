57eb6c3c397a51706928bdcd4d1408054272fe64
Philip Beadle <philip.beadle@live.com.au>
2020-10-12 04:38:22 +1100

    Messages retrieved from holochain when Channel opened, and continuous 1 sec polling

--- src/store/index.js
+++ src/store/index.js
@@ -8,59 +8,86 @@ import { getPersistedState } from "./stateMapper";
 Vue.use(Vuex);
 const ADMIN_PORT = 3301;
 const APP_ID = "ElementalChat";
+const today = new Date();
+const dd = String(today.getUTCDate());
+const mm = String(today.getUTCMonth() + 1); //January is 0!
+const yyyy = String(today.getUTCFullYear());
 export default new Vuex.Store({
-  state: {},
+  state: {
+    connectedToHolochain: false,
+    today: { year: yyyy, month: mm, day: dd }
+  },
   mutations: {
-    initialiseStore(state) {
-      getPersistedState("setAgentKey").then(
-        agentKey => (state.agentKey = agentKey)
-      );
-      getPersistedState("setAppInterface").then(
-        appInterface => (state.appInterface = appInterface)
-      );
-      console.log(state);
-    },
     setAgentKey(state, payload) {
+      state.agentKey = payload;
+    },
+    setPersistedAgentKey(state, payload) {
       state.agentKey = payload;
     },
     setAppInterface(state, payload) {
       state.appInterface = payload;
     },
+    setPersistedAppInterface(state, payload) {
+      state.appInterface = payload;
+    },
     setHolochainClient(state, payload) {
       state.holochainClient = payload;
+      state.connectedToHolochain = true;
     }
   },
   actions: {
-    initialiseAgent: async ({ commit, state }) => {
-      if (state.agentKey === undefined) {
-        const admin = await AdminWebsocket.connect(
-          `ws://localhost:${ADMIN_PORT}`
-        );
-        const agentKey = await admin.generateAgentPubKey();
-        commit("setAgentKey", agentKey);
-        const app = await admin.installApp({
-          app_id: APP_ID,
-          agent_key: agentKey,
-          dnas: [
-            {
-              path:
-                "/Users/philipbeadle/holochain-rsm/elemental-chat/elemental-chat.dna.gz",
-              nick: "Elemental Chat"
-            }
-          ]
-        });
-        const cellId = app.cell_data[0][0];
-        await admin.activateApp({ app_id: APP_ID });
-        const { port } = await admin.attachAppInterface({ port: 0 });
-        commit("setAppInterface", { port, cellId });
-        const client = await AppWebsocket.connect(`ws://localhost:${port}`);
-        commit("setHolochainClient", client);
-      } else {
-        const client = await AppWebsocket.connect(
-          `ws://localhost:${state.appInterface.port}`
-        );
-        commit("setHolochainClient", client);
-      }
+    initialiseAgent({ commit }) {
+      getPersistedState("setAgentKey").then(agentKey => {
+        console.log(agentKey);
+        if (agentKey === undefined || agentKey === null) {
+          AdminWebsocket.connect(`ws://localhost:${ADMIN_PORT}`).then(admin => {
+            admin.generateAgentPubKey().then(agentKey => {
+              commit("setAgentKey", agentKey);
+              admin
+                .installApp({
+                  app_id: APP_ID,
+                  agent_key: agentKey,
+                  dnas: [
+                    {
+                      path:
+                        "/Users/philipbeadle/holochain-rsm/elemental-chat/elemental-chat.dna.gz",
+                      nick: "Elemental Chat"
+                    }
+                  ]
+                })
+                .then(app => {
+                  const cellId = app.cell_data[0][0];
+                  admin.activateApp({ app_id: APP_ID });
+                  admin.attachAppInterface({ port: 0 }).then(appInterface => {
+                    commit("setAppInterface", {
+                      port: appInterface.port,
+                      cellId
+                    });
+                    AppWebsocket.connect(
+                      `ws://localhost:${appInterface.port}`
+                    ).then(client => {
+                      commit("setHolochainClient", client);
+                    });
+                  });
+                });
+            });
+          });
+        } else {
+          commit("setPersistedAgentKey", agentKey);
+          getPersistedState("setAppInterface").then(appInterface => {
+            commit("setPersistedAppInterface", {
+              port: appInterface.port,
+              cellId: appInterface.cellId
+            });
+            AppWebsocket.connect(`ws://localhost:${appInterface.port}`).then(
+              client => {
+                console.log("persist");
+                commit("setHolochainClient", client);
+              }
+            );
+          });
+        }
+      });
     }
   },
   modules: {
