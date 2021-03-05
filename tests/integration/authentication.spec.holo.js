cec5dbd36f099477d0c3bf1e375d97d94d37f803
jetttech <lisa.jetton@holo.host>
2021-03-04 18:43:38 -0600

    remove logs

--- tests/integration/authentication.spec.holo.js
+++ tests/integration/authentication.spec.holo.js
@@ -1,15 +1,7 @@
-import App from '@/App.vue'
-
 // dummy placeholder test
 describe("Message.vue", () => {
   it("renders props.msg when passed", () => {
-    console.log("App: ", App);
+    console.log("This is a placeholder test for the integration tests on holo ");
     expect(true).toBe(true);
   });
 });
-
-// const addChannelBtn = await page.waitForSelector('.mb-1 > .v-toolbar__content > #add-channel > .v-btn__content > .mdi-chat-plus-outline')
-// await page.click(addChannelBtn)
-
-// const addChannelSubmitBtn = await page.waitForSelector('.v-input #channel-name')
-// await page.click(addChannelSubmitBtn)
