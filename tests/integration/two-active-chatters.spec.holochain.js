6a93e67d39a4823afdf8013c188be7f6a418e911
Lisa Jetton <ldjetton21@gmail.com>
2021-03-18 13:31:53 -0500

    Update tests/integration/two-active-chatters.spec.holochain.js
    Co-authored-by: Robbie Carlton <robbie.carlton@gmail.com>

--- tests/integration/two-active-chatters.spec.holochain.js
+++ tests/integration/two-active-chatters.spec.holochain.js
@@ -106,7 +106,7 @@ orchestrator.registerScenario('Two Active Chatters', async scenario => {
       expect(newChannelElement).toBeTruthy()
       expect(newChannelHTML).toContain(newChannelTitle())
 
-      // alice (as tryorama node) verifies new message is in list of messages from the dht
+      // alice (as tryorama node) verifies new channel is in list of channels from the dht
       const callListChannels = async () => await aliceChat.call('chat', 'list_channels', { category: 'General' })
       const { channels: newChannels } = await awaitZomeResult(callListChannels, 90000, 10000)
       channelInFocus = newChannels.find(channel => channel.info.name === newChannelTitle())
