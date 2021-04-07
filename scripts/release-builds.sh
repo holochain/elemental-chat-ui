492384f57f9767400ae765b10dee2bbed421189d
Eric Harris-Braun <eric@harris-braun.com>
2021-04-07 12:14:13 -0400

    Fix version number and bump dna version

--- scripts/release-builds.sh
+++ scripts/release-builds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 build () {
-    sed -i "s/const DNA_UUID = '.*'/const DNA_UUID = '$2'/" src/consts.js
     sed -i "s/const DNA_VERSION = '.*'/const DNA_VERSION = '$1'/" src/consts.js
+    sed -i "s/const DNA_UUID = '.*'/const DNA_UUID = '$2'/" src/consts.js
     npm run build:self-hosted
     cd dist
     rm service-worker.js
@@ -10,6 +10,6 @@ build () {
     rm elemental-chat-for-dna-$1-$2.zip
     mv dist/elemental-chat.zip elemental-chat-for-dna-$1-$2.zip
 }
-build 0_1_0_alpha2 0002
-build 0_1_0_alpha2 0001
-build 0_1_0_alpha2 develop
+build 0_1_0_alpha3 0002
+build 0_1_0_alpha3 0001
+build 0_1_0_alpha3 develop
