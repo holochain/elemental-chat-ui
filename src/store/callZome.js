239475a899d06decaefba1ac34a5e16bcb183e3b
jetttech <ldjetton21@gmail.com>
2021-06-03 22:15:55 -0500

    increase display time for message.  Update log with wait time.

--- src/store/callZome.js
+++ src/store/callZome.js
@@ -81,12 +81,11 @@ export const callZome = async (dispatch, rootState, zomeName, fnName, payload, t
   } catch (e) {
     log(`${zomeName}.${fnName} ERROR: callZome threw error`, e)
     if (e.toString().includes('membrane proof') && e.toString().includes('already used')) {
-      console.log(' ERROR >>>>>>>>>>>>>>>>>>>>', e)
       const uniqueSigningCodeFailureMsg = 'Sign-up has failed and the automatic sign-out has been triggered because the provided joining code was already used.  Please reattempt sign-up with a unique joining code or sign-in if a returning user.'
       dispatch('setErrorMessage', uniqueSigningCodeFailureMsg, { root: true })
       // wait for error message to show
-      await wait(2000)
-      console.error('Signed up with previously used jonining code - will now sign user out.')
+      await wait(15000)
+      console.error('Signed up with previously used jonining code - will sign user out after 15 seconds.')
       return await dispatch('holochain/holoLogout', null, { root: true })
     } else if (e.toString().includes('Socket is not open') || e.toString().includes('Socket not ready') || e.toString().includes('Waited for')) {
       log('Socket is not open. Resetting connection state...')
