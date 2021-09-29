120205431a27bf9bb2ee65547a14a5ba8d2b070f
timotree3 <timorcb@gmail.com>
2021-09-29 19:49:56 -0400

    Update tuning params

--- tests/integration/setup/tryorama.js
+++ tests/integration/setup/tryorama.js
@@ -27,13 +27,25 @@ const network = {
     }
   ],
   tuning_params: {
-    gossip_loop_iteration_delay_ms: 200, // default 10
-    default_notify_remote_agent_count: 5, // default 5
-    default_notify_timeout_ms: 100, // default 1000
-    default_rpc_single_timeout_ms: 20000, // default 2000
-    default_rpc_multi_remote_agent_count: 2, // default 2
-    default_rpc_multi_timeout_ms: 2000, // default 2000
-    agent_info_expires_after_ms: 1000 * 60 * 20 // default 1000 * 60 * 20 (20 minutes)
+    gossip_strategy: "sharded-gossip",
+    gossip_loop_iteration_delay_ms: 2000,
+    default_notify_remote_agent_count:  5,
+    default_notify_timeout_ms: 1000 * 30,
+    default_rpc_single_timeout_ms: 1000 * 30,
+    default_rpc_multi_remote_agent_count: 1,
+    default_rpc_multi_timeout_ms: 1000 * 30,
+    agent_info_expires_after_ms: 1000 * 60 * 20,
+    tls_in_mem_session_storage: 512,
+    proxy_keepalive_ms: 1000 * 60 * 2,
+    proxy_to_expire_ms: 1000 * 60 * 5,
+    concurrent_limit_per_thread: 4096,
+    tx2_quic_max_idle_timeout_ms: 1000 * 30,
+    tx2_pool_max_connection_count: 4096,
+    tx2_channel_count_per_connection: 16,
+    tx2_implicit_timeout_ms: 1000 * 30,
+    tx2_initial_connect_retry_delay_ms: 200,
+    default_rpc_multi_remote_request_grace_ms: 10,
+    gossip_single_storage_arc_per_space: true
   }
 }
 
