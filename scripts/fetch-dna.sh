57dfe1430de389c987dcb72252b85e89dccc9943
Robbie Carlton <robbie.carlton@gmail.com>
2021-11-10 10:36:18 -0700

    Update scripts/fetch-dna.sh
    Co-authored-by: Timo <timorcb@gmail.com>

--- scripts/fetch-dna.sh
+++ scripts/fetch-dna.sh
@@ -1,2 +1,3 @@
 
+# Note: DNA specified here must have membrane proofs disabled
 curl -LJ https://github.com/holochain/elemental-chat/releases/download/v0.2.1-alpha2/elemental-chat.dna -o ./dnas/elemental-chat.dna
