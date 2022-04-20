f4f120e31b637d6183d119aad0957fe7004929c3
Robbie Carlton <robbie.carlton@gmail.com>
2022-04-20 13:35:33 -0600

    Bump holonix version

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/8cfa69e6c3069e9efbda32cff39d5e9571dcec9a.tar.gz";
+  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/1ea4c88f05557a5cea0e507c36eeb6f03bd29b54.tar.gz";
   holonix = import (holonixPath) {
     holochainVersionId = "custom";
     holochainVersion = import ./holochain_version.nix;
