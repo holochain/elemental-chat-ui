73adc640be1ec919eb14a464ca1755ad9209c13b
jetttech <ldjetton21@gmail.com>
2022-05-19 18:36:22 -0500

    remove consoles, add chromium logger

--- config/puppeteer/setup.js
+++ config/puppeteer/setup.js
@@ -8,7 +8,7 @@ const os = require('os')
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 module.exports = async function () {
-  const browser = await puppeteer.launch({ headless: true, args: ['--disable-features=site-per-process', '–no-sandbox', '–disable-setuid-sandbox', '--disable-extensions'] })
+  const browser = await puppeteer.launch({ dumpio: true, headless: true, args: ['--disable-features=site-per-process', '–no-sandbox', '–disable-setuid-sandbox', '--disable-extensions'] })
 
   // store the browser instance so we can teardown it later
   // this global is only available in the teardown but not in TestEnvironments
