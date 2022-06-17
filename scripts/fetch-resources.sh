63982bf974f9b8ee0b2416651de873596aa26b7d
Robbie Carlton <robbie.carlton@gmail.com>
2022-06-17 13:28:44 -0600

    Use zipped holo-dev-server

--- scripts/fetch-resources.sh
+++ scripts/fetch-resources.sh
@@ -4,6 +4,11 @@ curl -LJ https://github.com/holochain/elemental-chat/releases/download/v0.2.6/el
 curl -LJ https://github.com/holochain/elemental-chat/releases/download/v0.2.6/elemental-chat.happ -o ./dnas/elemental-chat.happ
 curl -LJ https://holo-host.github.io/servicelogger-rsm/releases/downloads/0_2_2/servicelogger.0_2_2.happ -o ./dnas/servicelogger.happ
 curl -LJ https://holo-host.github.io/servicelogger-rsm/releases/downloads/0_2_2/servicelogger.0_2_2.happ -o ./dnas/servicelogger.happ
-curl -LJ https://holo-host.github.io/envoy-chaperone/releases/0_1_0/holo-dev-server-x86_64-unknown-linux-gnu.v0_1_0 -o ./tmp/holo-dev-server
+curl -LJ https://holo-host.github.io/envoy-chaperone/releases/0_1_0/holo-dev-server-x86_64-unknown-linux-gnu.tar.gz -o ./tmp/holo-dev-server.tar.gz
 
-chmod +x ./tmp/holo-dev-server
+cd tmp
+
+tar -xf ./holo-dev-server.tar.gz holo-dev-server-x86_64-unknown-linux-gnu/holo-dev-server
+mv ./holo-dev-server-x86_64-unknown-linux-gnu/holo-dev-server ./holo-dev-server
+rm -r ./holo-dev-server-x86_64-unknown-linux-gnu
+rm -r ./holo-dev-server.tar.gz
